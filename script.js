39bc372ea9c659cf24f672377150cdddca39a1c1
XxENGAGExX <gamehacks101@hotmail.com>
2024-10-11 11:19:28 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript for any interactive features can be added here
+
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
